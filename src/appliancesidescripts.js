89c0886aa1957a300d80df3230a5191f5c4c31ac
peatearg <gallagherpeter@icloud.com>
2022-08-12 18:00:36 -0400

    added hyperlinks to the entitlements

--- src/appliancesidescripts.js
+++ src/appliancesidescripts.js
@@ -17,13 +17,51 @@ class appliancesidescripts {
   getTreeItem(element){
     return element;
   }
-  groupEntitlementScripts(){
+  async groupEntitlementScripts(){
     this.host = this.entitlementScript.filter(f=> f.type === 'host')
     this.portOrType = this.entitlementScript.filter(f=> f.type === 'portOrType')
     this.appShortcut = this.entitlementScript.filter(f=> f.type === 'appShortcut')
+    this.entitlements = await this.session.customGet('entitlements')
   }
- getChildren(element){
-  this.groupEntitlementScripts()
+  countbyhost(scriptname){
+    let count = this.entitlements.filter(entitlement=>
+      //console.log(entitlement)
+      entitlement.actions.some(action=>
+        //console.log(action.hosts.includes(`script://${scriptname}`))
+        action.hosts.includes(`script://${scriptname}`)
+      )
+      //return acc + val.actions.filter(f => f.hosts.includes(`script://${scriptname}`))}, [])
+  )
+return count
+}
+
+countbyportOrType(scriptname){
+  let count = this.entitlements.filter(entitlement=>
+    //console.log(entitlement)
+    entitlement.actions.some(action=>{
+        return action.hasOwnProperty('ports')  ? action.ports.includes(`script://${scriptname}`) : action.hasOwnProperty('types')  ? action.types.includes(`script://${scriptname}`) : undefined; 
+    }
+        //action.hasOwnProperty('types') ? action.types.includes(`script://${scriptname}`) : []
+    ))
+        //|| action.types.includes(`script://${scriptname}`) 
+    
+    //return acc + val.actions.filter(f => f.hosts.includes(`script://${scriptname}`))}, [])
+
+return count
+}
+
+countbyappShortcut(scriptid){
+  let count = this.entitlements.filter(entitlement=>
+    //console.log(entitlement)
+    entitlement.appShortcutScripts.includes(scriptid)
+    )
+    //return acc + val.actions.filter(f => f.hosts.includes(`script://${scriptname}`))}, [])
+return count
+}
+
+ async getChildren(element){
+  
+  await this.groupEntitlementScripts()
     if(element){
       let array = [];
       if(element.label === 'entitlementScript'){
@@ -39,11 +77,21 @@ class appliancesidescripts {
         for (let i of element['toExpand']){
             let subclaim = new vscode.TreeItem(i['name'], vscode.TreeItemCollapsibleState.None)
             subclaim['toExpand'] = i['expression']
-            subclaim['scriptType'] = element.contextValue;
-            subclaim.contextValue = 'script';
+            subclaim['scriptType'] = i.type;
+            if(i.type === 'appShortcut'){
+              subclaim.contextValue = this.countbyappShortcut(i['id']);
+            }else if(i.type === 'host'){
+              subclaim.contextValue = this.countbyhost(i['name'])
+            }else if(i.type === 'portOrType'){
+              subclaim.contextValue = this.countbyportOrType(i['name'])
+            }
+            subclaim.tooltip = new vscode.MarkdownString();
+            subclaim.label += ` (${subclaim.contextValue.length})`
+            subclaim.contextValue.forEach(e=>subclaim.tooltip.appendMarkdown(`[${e.name}](https://controller.sdpfederal.com:8443/ui/access/entitlements/edit/${e.id})  \n`))
             subclaim.iconPath = new vscode.ThemeIcon(scripticonmap[element.contextValue]);
             array.push(subclaim)
         }}
+        console.log(array)
         return array
       }
       else {
